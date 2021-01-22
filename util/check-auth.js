3fe1dcde1dc73c003ec61b4072103d66b0369466
Patrick Sheriddan <sheridp2@gmail.com>
2021-01-21 16:40:38 -0800

    deploy

--- util/check-auth.js
+++ util/check-auth.js
@@ -1,7 +1,7 @@
 const { AuthenticationError } = require("apollo-server");
 
 const jwt = require("jsonwebtoken");
-const { SECRET_KEY } = require("../config/keys");
+const { SECRET_KEY } = require("../config/prod");
 
 module.exports = (context) => {
   const authHeader = context.req.headers.authorization;
